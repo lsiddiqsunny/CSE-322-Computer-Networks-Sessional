9e7744fa5b0d0850297a3197f053cf25cbfb87f4
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2018-12-05 23:47:48 +0600

    Update router.cpp

--- Assignment 2-DVR Implementation/Assignment 2-DVR Implementation/router.cpp
+++ Assignment 2-DVR Implementation/Assignment 2-DVR Implementation/router.cpp
@@ -6,46 +6,47 @@
 #define ip_size 36
 #define bufsz 1024
 #define inf 1000000
+#define MAXRUT 100
 
 using namespace std;
-struct sockaddr_in target_address;
+
 struct sockaddr_in own_address;
 int sockfd;
 int bind_flag;
-void setup_target(char *target_ip){
 
-	target_address.sin_family = AF_INET;
-	target_address.sin_port = htons(4747);
-	target_address.sin_addr.s_addr = inet_addr(target_ip);
-
-}
-
-void setup_own(char *own_ip){
-	
-	own_address.sin_family = AF_INET;
-	own_address.sin_port = htons(4747);
-	own_address.sin_addr.s_addr = inet_addr(own_ip);
-
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	bind_flag = bind(sockfd, (struct sockaddr*) &own_address, sizeof(sockaddr_in));
-	printf("Connection stablished\n");
-
-}
 struct Router{
 	char ip_addr[ip_size];
 	int cost;
+	struct sockaddr_in address;
+	int up;
 	Router(char *ip,int c){
 		strcpy(ip_addr,ip);
 		cost=c;	
-		//printf("%s\n",ip_addr);
+		up=1;
+		address.sin_family = AF_INET;
+		address.sin_port = htons(4747);
+		address.sin_addr.s_addr = inet_addr(ip);
+		
+
 	}
 	bool match(char *ano_ip){
 		return !strcmp(ip_addr,ano_ip);
 	}
 
 };
+void setup_own(Router cur){
+	
+	
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	bind_flag = bind(sockfd, (struct sockaddr*) &cur.address, sizeof(sockaddr_in));
+	printf("Connection stablished\n");
+
+}
 
 struct Routing_Table{
+	Router router[MAXRUT];
+	
 	
 
 };
@@ -59,8 +60,8 @@ int main(int argc, char *argv[]){
 		printf("Wrong input format.\n Right format : %s <ip address> topo.txt\n", argv[0]);
 		exit(1);
 	}
-	setup_own(argv[1]);
-	Router router1("192.168.10.2",5);
+	Router Cur(argv[1],0);
+	setup_own(Cur);
 	
 	
 

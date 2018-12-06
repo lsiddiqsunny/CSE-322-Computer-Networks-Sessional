7a7fe5f5d5561cf9a7ea875408ac802de682dae5
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2018-12-06 10:50:54 +0600

    Offline 2 : Constructing routing table and showing

--- Assignment 2-DVR Implementation/Assignment 2-DVR Implementation/router.cpp
+++ Assignment 2-DVR Implementation/Assignment 2-DVR Implementation/router.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 #define ip_size 36
 #define bufsz 1024
 #define inf 1000000
@@ -13,9 +13,11 @@ using namespace std;
 struct sockaddr_in own_address;
 int sockfd;
 int bind_flag;
+char server_ip[ip_size]="192.168.10.100";
 
 struct Router{
 	char ip_addr[ip_size];
+	char nexthop_addr[ip_size];
 	int cost;
 	struct sockaddr_in address;
 	int up;
@@ -40,28 +42,102 @@ void setup_own(Router cur){
 
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	bind_flag = bind(sockfd, (struct sockaddr*) &cur.address, sizeof(sockaddr_in));
-	printf("Connection stablished\n");
+	//printf("Connection stablished\n");
 
 }
 
-struct Routing_Table{
-	Router router[MAXRUT];
-	
-	
+vector<Router>routers;
+int  inrouter(char *check_ip){
+	for(int i=0;i<routers.size();i++){
+		if(routers[i].match(check_ip)){
+			return i;
+		}
+	}
+	return -1;
 
-};
+}
+void show_routing_table(){
+	printf("Destination    Next Hop       Cost\n");
+	printf("-----------   -----------  ...........\n");
+	for(int i=0;i<routers.size();i++){
+		printf("%s  %s     %d\n",routers[i].ip_addr,routers[i].nexthop_addr,routers[i].cost);
+	}
+}
 int main(int argc, char *argv[]){
 
 	
 	char buffer[bufsz];
+	FILE *topo;
+	char delim[]=" ";
 	
 
-	if(argc < 2){
+	if(argc < 3){
 		printf("Wrong input format.\n Right format : %s <ip address> topo.txt\n", argv[0]);
 		exit(1);
 	}
 	Router Cur(argv[1],0);
+	Router Server(server_ip,inf);
 	setup_own(Cur);
+	topo=fopen(argv[2],"r");
+	if(topo==NULL){
+		printf("File can not open : %s\n",argv[2]);
+		exit(1);
+	}
+	
+	while(fgets(buffer,bufsz,topo)!=NULL){
+		char *ptr=strtok(buffer,delim);
+		char *src;
+		char *dest;
+		int cost;
+		int i=0;
+		while(ptr!=NULL){
+			if(i==0) src=ptr;
+			else if(i==1) dest=ptr;
+			else cost=atoi(ptr);
+			i++;
+			ptr=strtok(NULL,delim);
+		}
+		if(strcmp(argv[1],src)==0){
+			int x=inrouter(dest);
+			if(x==-1){
+				Router new_router(dest,cost);
+				strcpy(new_router.nexthop_addr,dest);
+				routers.push_back(new_router);
+			}else{
+				routers[x].cost=cost;
+				strcpy(routers[x].nexthop_addr,dest);
+			}
+			
+		}else if(strcmp(argv[1],dest)==0){
+			int x=inrouter(src);
+			if(x==-1){
+				Router new_router(src,cost);
+				strcpy(new_router.nexthop_addr,src);
+				routers.push_back(new_router);
+			}else{
+				routers[x].cost=cost;
+				strcpy(routers[x].nexthop_addr,src);
+			}
+
+		}else{
+			int x=inrouter(src);
+			if(x==-1){
+				Router new_router(src,inf);
+				strcpy(new_router.nexthop_addr, "  -------   ");
+				routers.push_back(new_router);
+			}
+			x=inrouter(dest);
+			if(x==-1){
+				Router new_router(dest,inf);
+				strcpy(new_router.nexthop_addr, "  -------  ");
+				routers.push_back(new_router);
+			}
+
+		}
+
+	}
+	show_routing_table();
+	fclose(topo);
 	
 	
 

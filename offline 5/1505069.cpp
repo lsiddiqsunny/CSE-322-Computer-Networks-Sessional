290db710db96d8061db6bb810a6a1b1fe0f7d35d
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2019-01-31 21:52:18 +0600

    Finished offline 5

--- offline 5/1505069.cpp
+++ offline 5/1505069.cpp
@@ -7,6 +7,7 @@ int m;
 double p;
 string poly;
 
+
 bool isPowerofTwo(int x)
 {
     while(x%2==0)
@@ -45,7 +46,7 @@ int main()
     cout<<"\n\nData string after padding : "<<s<<endl<<endl;
 
 
-    cout<<"data block <ascii code of m characters per row>: "<<endl;
+    cout<<"data block <ascii code of m characters per row> : "<<endl;
     int sz=s.size();
     vector<int>datablock[sz/m];
     int k=-1;
@@ -82,7 +83,7 @@ int main()
     {
         for(int j=0; j<datablock[i].size(); j++)
         {
-                cout<<datablock[i][j];
+            cout<<datablock[i][j];
 
         }
         cout<<endl;
@@ -93,13 +94,17 @@ int main()
 
     for(int i=0; i<(sz/m); i++)
     {
-       // cout<<i<<endl;
-        int total=m+2;
+        // cout<<i<<endl;
+        int total=m+3;
         for(int j=0; j<total; j++)
         {
             int mask=1;
             mask<<=j;
             mask--;
+           // cout<<mask<<endl;
+           if(mask>datablock[i].size()){
+            continue;
+           }
             datablock[i].insert(datablock[i].begin()+mask,0);
 
         }
@@ -108,7 +113,7 @@ int main()
             int mask=1;
             mask<<=k;
             int parity=0;
-           // cout<<k<<endl;
+            // cout<<k<<endl;
             for(int j=0; j<datablock[i].size(); j++)
             {
                 if(((j+1)&mask)!=0)
@@ -118,7 +123,7 @@ int main()
                 }
 
             }
-           // cout<<endl;
+            // cout<<endl;
             mask--;
             datablock[i][mask]=parity;
         }
@@ -151,15 +156,268 @@ int main()
         cout<<endl;
     }
 
+    cout<<"\ndata bits after column-wise serialization : "<<endl;
+    vector<int>serializeddata;
+    for(int i=0; i<(m*8+m+2); i++)
+    {
+        for(int j=0; j<(sz/m); j++)
+        {
+            serializeddata.push_back(datablock[j][i]);
+        }
+    }
+    for(int i=0; i<serializeddata.size(); i++)
+    {
+        cout<<serializeddata[i];
+    }
+    cout<<endl<<endl;
+
+    vector<int>remserializeddata;
+    remserializeddata=serializeddata;
+    cout<<"data bits after appending CRC checksum <send frame> : "<<endl;
+    int sz1=poly.size();
+    for(int i=1; i<sz1; i++)
+    {
+        serializeddata.push_back(0);
+    }
+    for(int i=0; i<=serializeddata.size()-sz1; i++)
+    {
+        if(serializeddata[i]==0)
+            continue;
+        for(int j=i; j<i+sz1; j++)
+        {
+            serializeddata[j]^=(poly[j-i]-'0');
+        }
+    }
+
+    vector<int>reminder;
+    bool ok=false;
+    for(int i=0; i<serializeddata.size(); i++)
+    {
+        if(serializeddata[i]==1)
+            ok=true;
+        if(!ok)
+            continue;
+
+        reminder.push_back(serializeddata[i]);
+    }
+    serializeddata=remserializeddata;
+    for(int i=0; i<reminder.size(); i++)
+    {
+        serializeddata.push_back(reminder[i]);
+    }
+    //cout<<serializeddata.size()<<" "<<sz<<endl;
+    for(int i=0; i<serializeddata.size(); i++)
+    {
+        if(i>=((sz/m)*(m+2+m*8)))
+        {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+
+        }
+        cout<<serializeddata[i];
+    }
+    cout<<endl;
+    SetConsoleTextAttribute ( h, wOldColorAttrs);
+    cout<<"\nreceived frame : "<<endl;
+    int havetotoggle=ceil((serializeddata.size())*(p));
+    int framesize=serializeddata.size();
+    vector<int>pos;
+    srand(time(NULL));
+    for(int i=1; i<=havetotoggle; i++)
+    {
+        int x=rand()%framesize;
+        int co=0;
+        while(find(pos.begin(),pos.end(),x)!=pos.end())
+        {
+            co++;
+            if(co>=(framesize/havetotoggle))
+            {
+                break;
+            }
+            x=rand()%framesize;
+        }
+        pos.push_back(x);
+    }
+    for(int i=0; i<pos.size(); i++)
+    {
+        //cout<<pos[i]<<endl;
+        serializeddata[pos[i]]^=1;
+    }
+
+    for(int i=0; i<serializeddata.size(); i++)
+    {
+        if(find(pos.begin(),pos.end(),i)!=pos.end())
+        {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+
+        }
+        else
+        {
+            SetConsoleTextAttribute ( h, wOldColorAttrs);
+
+        }
+        cout<<serializeddata[i];
+    }
+    SetConsoleTextAttribute ( h, wOldColorAttrs);
+    cout<<endl;
+
+    ok=true;
+    remserializeddata=serializeddata;
+    for(int i=0; i<=serializeddata.size()-sz1; i++)
+    {
+        if(serializeddata[i]==0)
+            continue;
+        for(int j=i; j<i+sz1; j++)
+        {
+            serializeddata[j]^=(poly[j-i]-'0');
+        }
+    }
+    for(int i=0; i<serializeddata.size(); i++)
+    {
+        if(serializeddata[i])
+        {
+            ok=false;
+        }
+    }
+
+    cout<<"\nresult of CRC checksum matching : ";
+    if(ok)
+    {
+        cout<<"no error detected"<<endl;
+    }
+    else
+        cout<<"error detected"<<endl;
+
+    cout<<"\ndata block after removing CRC checksum bits : "<<endl;
+
+    vector<int>datablock1[sz/m];
+    vector<int>receiveddata[sz/m];
+    serializeddata.clear();
+    for(int i=0; i<((sz/m)*(m+2+m*8)); i++)
+    {
+
+        serializeddata.push_back(remserializeddata[i]);
+    }
+    int l=sz/m;
+    for(int i=0; i<l; i++)
+    {
+        for(int k=0; k<m+2+m*8; k++)
+        {
+            datablock1[i].push_back(serializeddata[i+l*k]);
+        }
+    }
+    for(int i=0; i<l; i++)
+    {
+        for(int j=0; j<datablock1[i].size(); j++)
+        {
+            int k=(i+l*j);
+
+
+            if(find(pos.begin(),pos.end(),k)!=pos.end())
+            {
+                //cout<<k<<endl;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+
+            }
+            else
+            {
+                SetConsoleTextAttribute ( h, wOldColorAttrs);
+
+            }
+            cout<<datablock1[i][j];
+        }
+        cout<<endl;
+    }
+    SetConsoleTextAttribute ( h, wOldColorAttrs);
+    cout<<"\ndata block after removing checkbits : "<<endl;
 
 
 
 
 
 
+    for(int i=0; i<(sz/m); i++)
+    {
+        // cout<<i<<endl;
+        int total=m+2;
+        pos.clear();
+        for(int k=0; k<total; k++)
+        {
+            int mask=1;
+            mask<<=k;
+            int parity=0;
+            // cout<<k<<endl;
+            for(int j=0; j<datablock1[i].size(); j++)
+            {
+                if(((j+1)&mask)!=0)
+                {
+                    //cout<<j<<" ";
+                    parity^=datablock1[i][j];
+                }
+
+            }
+            //   cout<<parity<<" ";
+            pos.push_back(parity);
+            mask--;
+            //datablock[i][mask]=parity;
+        }
+        // cout<<endl;
+        int position=0;
+        int num=1;
+        for(int k=0; k<pos.size(); k++)
+        {
+            position+=(num*pos[k]);
+            num*=2;
+        }
+        position--;
+        if(position<0 || position>=(m*8+m+2))
+            continue;
+        // cout<<position<<endl;
+        datablock1[i][position]^=1;
+
+        //cout<<endl;
+
+    }
+    for(int i=0; i<sz/m; i++)
+    {
+        for(int j=0; j<datablock1[i].size(); j++)
+        {
+            if(isPowerofTwo(j+1))
+            {
+                continue;
+            }
+            receiveddata[i].push_back(datablock1[i][j]);
+        }
+
+    }
+
+    for(int i=0; i<sz/m; i++)
+    {
+        for(int j=0; j<receiveddata[i].size(); j++)
+        {
+            cout<<receiveddata[i][j];
+        }
+        cout<<endl;
+    }
+
+    cout<<"\noutput frame : ";
+    string ans="";
+    for(int i=0; i<sz/m; i++)
+    {
+        for(int j=0; j<receiveddata[i].size(); j=j+8)
+        {
+            bitset<8>ch;
+            for(int k=j; k<j+8; k++)
+            {
+                ch[7-k+j]=receiveddata[i][k];
+            }
+            ans.push_back((char)ch.to_ulong());
+
+        }
+
+    }
 
 
-
+    cout<<ans<<endl;
 
 }
 
@@ -168,4 +426,40 @@ Computer Networks
 4
 .04
 1010111
+*/
+/*
+no error
+3
+0
+101
+*/
+/*
+a
+1
+0
+101
+*/
+/*
+Hamming Code
+2
+.05
+10101
+*/
+/*
+Error Detection
+3
+.02
+10111
+*/
+/*
+many errors
+4
+.1
+10001
+*/
+/*
+no error
+3
+0
+101
 */
